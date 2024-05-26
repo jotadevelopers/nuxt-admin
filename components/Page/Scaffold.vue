b3e56aa9227f2747e779e0d89135dbe30a435d48
Josimar Costa <jotadevelopers@gmail.com>
2024-05-26 14:04:41 -0300

    fix(layout): adjusts the default layout to encapsulate the PageScaffold component

--- components/Page/Scaffold.vue
+++ components/Page/Scaffold.vue
@@ -7,18 +7,18 @@ const leftOpen = ref(true);
 
 </script>
 <template>
-    <div class="fixed w-full">
-        <div class="flex flex-row border-b items-center px-2">
+    <div class="stick">
+        <div class="w-full h-12 bg-white sticky top-12 flex flex-row gap-2 border-b z-10">
             <UButton v-if="$slots.left" class="my-2" @click="() => leftOpen = !leftOpen"
                 icon="i-heroicons-bars-3-16-solid" size="xs" variant="link">
             </UButton>
             <UHorizontalNavigation :links="props.navItems" class="" />
         </div>
         <div class="flex flex-row">
-            <div v-if="$slots.left" v-show="leftOpen" class="w-[220px] border-r h-screen p-1">
+            <div v-if="$slots.left" v-show="leftOpen" class="sticky top-24 w-[220px] p-1">
                 <slot name="left" />
             </div>
-            <div class="w-full p-1">
+            <div class="w-full h-auto p-2">
                 <slot />
             </div>
         </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,8 @@
     <div class="w-full min-h-screen bg-gray-50 dark:bg-slate-700" :class="`text-md`">
         <app-topbar></app-topbar>
         <app-sidebar></app-sidebar>
-        <main class="flex min-h-screen ml-12 pt-12 ">
-            <div class="w-full">
-                <slot />
-            </div>
+        <main class="mt-12 ml-12 w-auto ">
+            <NuxtPage />
         </main>
         <app-footer></app-footer>
         <USlideovers />
