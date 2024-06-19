33011da1d9389bd6c177d63c33b8573e8aa4c71a
Josimar Costa <jotadevelopers@gmail.com>
2024-06-19 20:02:06 -0300

    fix(component): fix show hide toolbar when haven´t content

--- components/Page/Scaffold.vue
+++ components/Page/Scaffold.vue
@@ -8,7 +8,7 @@ const leftOpen = ref(true);
 </script>
 <template>
     <div>
-        <div
+        <div v-if="$slots.left || props.navItems?.length"
             class="w-full h-12 bg-white dark:bg-slate-800 sticky top-12 flex flex-row gap-2 border-b dark:border-b-gray-600 z-10">
             <UButton v-if="$slots.left" class="my-2" @click="() => leftOpen = !leftOpen"
                 icon="i-heroicons-bars-3-16-solid" size="xs" variant="link">
@@ -19,7 +19,7 @@ const leftOpen = ref(true);
             <div v-if="$slots.left" v-show="leftOpen" class="w-[220px] p-2 fixed">
                 <slot name="left" />
             </div>
-            <div class="w-full p-2" :class="{ 'ml-[220px]': leftOpen }">
+            <div class="w-full p-2" :class="{ 'ml-[220px]': ($slots.left && leftOpen) }">
                 <slot />
             </div>
         </div>
